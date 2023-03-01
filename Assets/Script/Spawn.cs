aee6277755354fd53f6aa87a5cc31685ef40c78d
Sxea <nachothiago@hotmail.com>
2023-02-28 22:19:06 -0300

    Add List and Dictionary

--- Assets/Script/Spawn.cs
+++ Assets/Script/Spawn.cs
@@ -1,15 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
+public enum PrefabType
+{
+    Viking,
+    Warrior,
+    Egyptian,
+}
 public class Spawn : MonoBehaviour
 {
-    [SerializeField] private GameObject[] PreFab;
+    [SerializeField] private List<GameObject> PreFab;
     [SerializeField] private Transform spawnPoint;
-    
-   void Start()
+    [SerializeField] private int amountInstantiate = 4;
+    [SerializeField] private List<GameObject> viking , warrior , egyptian ;
+    [SerializeField] private PrefabType warriorSpawn;
+
+
+
+    private Dictionary<PrefabType, List<GameObject> > PrefabDictionary =
+        new Dictionary<PrefabType, List<GameObject>>();
+
+    private void PopulateDictionary()
     {
-        ChoosePrefab();
+        PrefabDictionary.Add(PrefabType.Viking, viking);
+        PrefabDictionary.Add(PrefabType.Warrior, warrior);
+        PrefabDictionary.Add(PrefabType.Egyptian, egyptian);
+    }
+
+    private void Awake()
+    {
+        /*for (int i =0; i < amountInstantiate; i++)
+        {
+            ChoosePrefab();
+        }*/
+        var preFabSpawn = PrefabDictionary[PrefabType.Warrior];
+        
+        /* NO FUNCIONA !
+          if (PrefabDictionary.TryGetValue(warriorSpawn, out var preFabToSpawn) )
+        {
+             preFabToSpawn;
+        }*/
+    }
+    void Start()
+    {
+        //ChoosePrefab();
     }
 
     
@@ -19,7 +55,7 @@ public class Spawn : MonoBehaviour
     }
     private void ChoosePrefab()
     {
-        var choosenPF = Random.Range(0, PreFab.Length);
+        var choosenPF = Random.Range(0, PreFab.Count);
         Instantiate(PreFab[choosenPF], spawnPoint.position,Quaternion.identity);
         Debug.Log("anduvo");
     }
